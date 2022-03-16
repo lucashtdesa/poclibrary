1118fc8804c9ac04e1924e0a18e0b3fb9e167f99
Lucas Henrique <68077602+lucashtdesa@users.noreply.github.com>
2022-03-16 11:57:21 -0300

    Add files via upload

---
+++ POCLibrary/Controllers/BookController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using POCLibrary.Models;
+using POCLibrary.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace POCLibrary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet("/search/{term}")]
+        public async Task<ActionResult<List<Books>>> GetBooksByTermAsync(string term)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var booklist = await _bookService.FindBooksByTermAsync(term);
+                if (booklist == null)
+                {
+                    return NotFound();
+                }
+                return Ok(booklist);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ POCLibrary/Models/Books.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace POCLibrary.Models
+{
+    public class Books
+    {
+        [JsonPropertyName("message")]
+        public string Message { get; set; }
+
+        [JsonPropertyName("error")]
+        public bool Error { get; set; }
+
+        [JsonPropertyName("statusCode")]
+        public long StatusCode { get; set; }
+
+        [JsonPropertyName("data")]
+        public Data Data { get; set; }
+    }
+
+    public partial class Data
+    {
+        [JsonPropertyName("suggestions")]
+        public List<string> Suggestions { get; set; }
+
+        [JsonPropertyName("books")]
+        public List<Book> Books { get; set; }
+    }
+
+    public partial class Book
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("link")]
+        public Uri Link { get; set; }
+
+        [JsonPropertyName("author")]
+        public string Author { get; set; }
+
+        [JsonPropertyName("price")]
+        public string Price { get; set; }
+
+        [JsonPropertyName("value")]
+        public float Value { get; set; }
+
+        [JsonPropertyName("image")]
+        public string Image { get; set; }
+    }
+}
