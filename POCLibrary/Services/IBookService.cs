50eb033e088ff260e3e0599ad5c21ac257790757
Lucas Henrique <68077602+lucashtdesa@users.noreply.github.com>
2022-03-16 11:58:25 -0300

    Add files via upload

---
+++ POCLibrary/Services/IBookService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using POCLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POCLibrary.Services
+{
+    public interface IBookService
+    {
+        public Task<IEnumerable<Books>> FindBooksByTermAsync(string term);
+    }
+}
---
+++ POCLibrary/Services/Implementation/BookServiceImplementation.cs
@@ -0,0 +1,29 @@
+﻿using POCLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace POCLibrary.Services.Implementation
+{
+    public class BookServiceImplementation : IBookService
+    {
+        private string _urlApi = "https://livrariacompasso.herokuapp.com/search?term=";
+        private readonly HttpClient _client = new HttpClient();
+        public async Task<IEnumerable<Books>> FindBooksByTermAsync(string term)
+        {
+            List<Books> booklist = new List<Books>();
+
+            var urlChamada = _urlApi + term;
+            var response = _client.GetStreamAsync(urlChamada);
+
+            var responseDeserialized = await JsonSerializer.DeserializeAsync<Books>(await response);
+            booklist.Add(responseDeserialized);
+            return booklist;
+        }
+    }
+}
